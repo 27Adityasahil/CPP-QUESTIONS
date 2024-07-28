fd45f347bd4ea57896872683ec31dd97a4e4869c
Aditya Raj <adityasahil24@gmail.com>
2024-07-28 12:55:09 +0530

    solve question 19

--- q18.cpp
+++ q18.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
 using namespace std;
 
+void rev(int arr[], int size){
+    int s = 0, e = size - 1;
+    while(s <= e){
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+}
+
+void printarray(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
 int main(){
-    int demo[100];
-    int size, temp;
+    int arr[10];
+    int size;
     cout<<"Enter the size of the array:: ";
     cin>>size;
 
+    cout<<"Enter the elements of the array:: ";
     for(int i = 0; i < size; i++){
-        cin>>demo[i];
+        cin>>arr[i];
     }
 
-    cout<<"The reversed array is:: "<<endl;
-    int start = 0, end = size - 1;
-    while(start <= end){
-        temp = demo[start];
-        demo[start] = demo[end];
-        demo[end] = temp;
-        start++;
-        end--;
-    }
-    for(int i = 0; i < size; i++){
-        cout<<demo[i]<<" ";
-    }
+    cout<<endl;
+
+    cout<<"The original array is:: \n";
+    printarray(arr, size);
+
+    cout<<endl;
+
+    rev(arr, size);
+    cout<<"The reversed array is:: \n";
+    printarray(arr, size);
+
+    return 0;    
 }
